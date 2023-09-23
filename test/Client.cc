90509a35bcd786f11132a48d2057ca475ab9da80
bytenan <bytenan@outlook.com>
2023-09-23 22:18:18 +0800

    完成了Connection模块的测试

--- test/Client.cc
+++ test/Client.cc
@@ -4,12 +4,15 @@ int main() {
 
     Socket client;
     client.CreateClient(8888, "127.0.0.1");
-    while (true) {
+    for (int i = 0; i < 5; ++i) {
         std::string msg = "hello world";
         client.Send(msg.c_str(), msg.size());
         char buf[1024];
         client.Recv(buf, sizeof(buf) - 1);
         DBG_LOG("%s", buf);
+        sleep(1);
+    }
+    while (true) {
         sleep(1);
     }
     client.Close();
--- test/Server.cc
+++ test/Server.cc
@@ -1,95 +1,52 @@
 #include "../source/Server.hpp"
 
-void Test1() {
-    Socket server;
-    server.CreateServer(8888);
-    while (true) {
-        int fd = server.Accept();
-        if (fd < 0) {
-            ERR_LOG("SERVER ACCEPT ERROR");
-            continue;
-        }
-        Socket sockfd(fd);
-        char buf[1024];
-        int ret = sockfd.Recv(buf, sizeof(buf) - 1);
-        if (ret < 0) {
-            sockfd.Close();
-            continue;
-        }
-        sockfd.Send(buf, ret);
-        sockfd.Close();
-    }
-    server.Close();
+std::unordered_map<int, ConnectionSharedPtr> conns;
+int id = rand() % 10000;
+
+void OnMessage(const ConnectionSharedPtr &conn, Buffer *buf) {
+    DBG_LOG("%s", buf->ReaderPosition());
+    buf->MoveReaderOffset(buf->ReadableSize());
+    std::string msg = "终于要胜利了！！！";
+    conn->Send(msg.c_str(), msg.size());
 }
 
-void Close(Channel *channel) {
-    std::cout << "close: " << channel->Fd() << std::endl;
-    channel->RemoveMonitor();
-    delete channel;
+void OnConnect(const ConnectionSharedPtr &conn) {
+    DBG_LOG("NEW CONNECTION: %p", conn.get());
 }
-void Read(Channel *channnel) {
-    char buf[1024];
-    int n = read(channnel->Fd(), buf, sizeof(buf) - 1);
-    if (n <= 0) {
-        Close(channnel);
-        return;
-    }
-    buf[n] = '\0';
-    std::cout << buf << std::endl;
-    channnel->EnableMonitorWriter();
+
+void ConnectionDestroy(const ConnectionSharedPtr &conn) {
+    conns.erase(conn->Id());
 }
-void Write(Channel *channel) {
-    std::string msg = "终于要成功了吗？";
-    int n = write(channel->Fd(), msg.c_str(), msg.size());
-    if (n < 0) {
-        Close(channel);
-        return;
-    }
-    channel->DisableMonitorWriter();
-}
-void Error(Channel *channel) {
-    Close(channel);
-}
-void Any(Channel *channel) {
-    std::cout << "有事件触发啦！！！" << std::endl;
-}
-void Acceptor(Epoller *epoller, int fd) {
+
+void Acceptor(EventLoop *loop, int fd) {
     int newfd = accept(fd, nullptr, nullptr);
     if (newfd < 0) return;
-    Channel *channel = new Channel(epoller, newfd);
-    channel->SetReadCallBack(std::bind(Read, channel));
-    channel->SetWriteCallBack(std::bind(Write, channel));
-    channel->SetCloseCallBack(std::bind(Close, channel));
-    channel->SetErrorCallBack(std::bind(Error, channel));
-    channel->SetAnyCallBack(std::bind(Any, channel));
-    channel->EnableMonitorRead();
+    ++id;
+    ConnectionSharedPtr conn(new Connection(loop, id, newfd));
+    conn->SetMessageCallBack(std::bind(OnMessage, std::placeholders::_1, std::placeholders::_2));
+    conn->SetConnectCallBack(std::bind(OnConnect, std::placeholders::_1));
+    conn->SetServerCloseCallBack(std::bind(ConnectionDestroy, std::placeholders::_1));
+    conn->EnableInactiveRelease(10);
+    conn->Established();
+    conns[id] = conn;
 }
 
-void Test2() {
+int main() {
+    srand(time(nullptr));
 
-    Epoller epoller;
+    EventLoop loop;
 
     Socket server;
     server.CreateServer(8888);
 
-    Channel channel(&epoller, server.Fd());
-    channel.SetReadCallBack(std::bind(Acceptor, &epoller, server.Fd()));
+    Channel channel(&loop, server.Fd());
+    channel.SetReadCallBack(std::bind(Acceptor, &loop, server.Fd()));
     channel.EnableMonitorRead();
 
     while(true) {
-        std::vector<Channel *> actives;
-        epoller.Epoll(&actives);
-        for (auto &channel : actives) {
-            channel->EventHandler();
-        }
+        loop.Run();
     }
     server.Close();
-}
-
-
-int main() {
-
-    Test2();
 
     return 0;
 }
